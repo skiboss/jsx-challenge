dc7d013fa8bb194065ef2058ebe7bf83f1135166
Oghenemeru G. Avwemoya <skiboss4life@gmail.com>
2022-11-28 20:15:50 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+//Create a react app from scratch.
+//It should display a h1 heading.
+//It should display an unordered list (bullet points).
+//It should contain 3 list elements.
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Ovwian 1 extollers</h1>
+    <ul>
+      <li>Oghenemeru Avwemoya</li>
+      <li>Caleb Mark</li>
+      <li>Stephen Sticks</li>
+      <li>Philips Ighanya</li>
+      <li>Raymond Ochuko</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
